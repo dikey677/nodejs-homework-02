a0cc7edcf16e87046b1c34a60de1a7831b5f57d9
Vyacheslav <dikey677@gmail.com>
2022-10-12 22:28:27 +0300

    fix var to const

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Вначале подключены все сторонние пакеты, которые нужны для функционирования приложения.
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// После мы подключаем роуты, в дальнейшем мы их изменим и внесем дополнительный функционал.
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+// Создается экземпляр приложения и подключаем шаблоны
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// После идет блок подключения промежуточного ПО (view engine setup).
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+// Подключается логгер, обработка JSON и данных из форм, и в конце модуль для работы с cookie.
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// После идет подключение роутеров в приложение.
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Помните, что порядок подключаемого промежуточного ПО имеет значение.
+// Вначале происходит обработка несуществующего роута или ошибка 404 (catch 404 and forward to error handler).
+app.use(function (req, res, next) {
+  next(createError(404)); // По сути отсутствие обработчика на запрашиваемый у сервера роутер это не ошибка и мы создаем ошибку и пробрасываем ее дальше для обработки.
 });
 
-// error handler
-app.use(function(err, req, res, next) {
+// В конце приложения идет обработка ошибок (error handler).
+// Здесь и происходит обработка ошибки. Мы пробрасываем переменные message и error в шаблон error.ejs и выполняем его рендер/
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
